efb60b2f13e37af58b322db3b3b8564c0b10bb65
md-fein <mikelfino7@gmail.com>
2026-02-22 15:25:27 -0500

    tripleNumbers and stringsToIntegers implemented

--- src/arrays.ts
+++ src/arrays.ts
@@ -20,7 +20,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((num) => num * 3);
 }
 
 /**
@@ -28,7 +28,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((str) => {
+        const num = parseInt(str);
+        return isNaN(num) ? 0 : num;
+    });
 }
 
 /**
