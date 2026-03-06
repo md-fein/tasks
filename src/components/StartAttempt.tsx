55c09a3fc2b3a015d5ea969722a26803a260aae2
md-fein <mikelfino7@gmail.com>
2026-03-05 22:06:21 -0500

    Implemented StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,37 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempt, setAttempt] = useState<number>(4);
+    const [inProgress, setProgress] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button
+                disabled={inProgress || attempt <= 0}
+                onClick={() => {
+                    setProgress(true);
+                    setAttempt(attempt - 1);
+                }}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                disabled={!inProgress}
+                onClick={() => {
+                    setProgress(false);
+                }}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                disabled={inProgress}
+                onClick={() => {
+                    setAttempt(attempt + 1);
+                }}
+            >
+                Mulligan
+            </Button>
+            <div>{attempt}</div>
+        </div>
+    );
 }
